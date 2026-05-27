73b61944b0fe77ccac15acec6556c5bb98b31279
naga <38852591+NaGaa95@users.noreply.github.com>
2026-05-28 01:40:13 +0200

    mangohud 0.8.4

--- scripts/mangohud.sh
+++ scripts/mangohud.sh
@@ -2,22 +2,33 @@
 
 echo "T210 MangoHud Fork script started!"
 
-package_available glslang-tools
-if [[ $? == "0" ]]; then
-  sudo apt install glslang-tools -y || error "Could not install apt dependencies"
-else
-  error "glslang-tools is not available so mangohud can not be compiled and installed"
-fi
+case "$__os_id" in
+Raspbian | Debian | Ubuntu)
+  package_available glslang-tools
+  if [[ $? == "0" ]]; then
+    sudo apt install glslang-tools -y || error "Could not install apt dependencies"
+  else
+    error "glslang-tools is not available so mangohud can not be compiled and installed"
+  fi
 
-sudo apt install ninja-build git build-essential cmake libx11-dev libwayland-dev libdbus-1-dev libxkbcommon-dev python3-mako -y || error "Could not install apt dependencies"
-if package_is_new_enough meson 0.60.0 ;then
-  sudo apt install -y meson || error "Could not install apt dependencies"
-else
-  pipx_install meson || exit 1
-fi
+  sudo apt install ninja-build git build-essential cmake pkg-config libx11-dev libwayland-dev libdbus-1-dev libxkbcommon-dev libgl-dev python3-mako -y || error "Could not install apt dependencies"
+  if package_is_new_enough meson 0.60.0 ;then
+    sudo apt install -y meson || error "Could not install apt dependencies"
+  else
+    pipx_install meson || exit 1
+  fi
+  ;;
+Fedora)
+  sudo dnf install -y --refresh @development-tools git cmake ninja-build meson pkgconf-pkg-config glslang libX11-devel wayland-devel dbus-devel libxkbcommon-devel mesa-libGL-devel python3-mako || error "Could not install dnf dependencies"
+  ;;
+*)
+  error "Unsupported distro detected - MangoHud install currently supports Ubuntu/Debian and Fedora"
+  ;;
+esac
+
 cd /tmp
 rm -rf MangoHud
-git clone https://github.com/theofficialgman/MangoHud.git --depth=1 -b master-l4t
+git clone https://github.com/NaGaa95/MangoHud.git --depth=1
 cd MangoHud
 meson build --prefix /usr -Dappend_libdir_mangohud=false -Dwith_xnvctrl=disabled || error "Could Not Configure Source"
 ninja -C build || error "Could Not Build Mangohud"
